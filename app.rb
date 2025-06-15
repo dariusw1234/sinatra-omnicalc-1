291fed5da732abb5af9ef593eeb6718904d5decb
Darius Woods <dariuswoods1919@gmail.com>
2025-06-15 22:58:14 +0000

    calculates squares, square roots, monthly payments, and chooses a random floating point number between a minimum and a maximum

--- app.rb
+++ app.rb
@@ -1,9 +1,56 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+#Square
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_f
+  @calc = @number * @number
+  @result = @calc.to_f
+  erb(:square_results)
+end
+
+#Square Root
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number").to_f
+  @result = Math.sqrt(@number)
+  erb(:square_root_results)
+end
+
+#Payment
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @r = @apr / 100 / 12
+  @years = params.fetch("years").to_i
+  @n = @years * 12
+  @principal = params.fetch("principal").to_f
+
+  @numerator = @r * @principal
+  @denominator = 1 - (1 + @r) ** -@n
+
+  @result = @numerator / @denominator
+  erb(:payment_results)
+end
+
+#Random
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @range = rand(@min..@max)
+  erb(:random_results)
 end
